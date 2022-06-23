66a3d25b1e9647e2084bf2d1a189895c60c73fb8
oberonSilni <sinadamov@gmail.com>
2022-06-24 01:33:49 +0200

    just beginning

---
+++ script.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+
+function Book(author, title, nrOfPages) {
+    this.author = author,
+    this.title = title,
+    this.nrOfPages = pages
+  }
+
+
+
+  function addBook(){
+    let book = new Book();
+    myLibrary.push(book);
+    
+  }
