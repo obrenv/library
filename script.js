601db2722cff7481104ca7734f0549ba494df0fc
oberonSilni <sinadamov@gmail.com>
2022-07-25 16:28:51 +0200

    bug fixes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let myLibrary = [];
+//Object constructor
 
+let myLibrary = [];
 
 function Book(author, title, nrOfPages) {
     this.author = author,
@@ -9,8 +10,60 @@ function Book(author, title, nrOfPages) {
 
 
 
-  function addBook(){
-    let book = new Book();
-    myLibrary.push(book);
-    
+
+//get form data
+let author = document.getElementById("author");
+let title = document.getElementById("title");
+let pages = document.getElementById("pages");
+
+let submit = document.getElementById("submitbtn");
+
+
+function addBook(){
+  let book = new Book(author.value, title.value, pages.value);
+  myLibrary.push(book);
+  modal.style.display = "none";
+  console.log("wut")
+  
+}
+
+
+
+
+//Modal
+
+
+let modal = document.getElementsByClassName("modalBck") [0];
+let dugmic = document.getElementById("dugme");
+let close = document.getElementsByClassName("close") [0];
+
+  dugmic.onclick = function() {
+    modal.style.display = "block";
+
   }
+
+close.onclick = function(){
+  modal.style.display = "none";
+}
+
+function render(){
+let para = document.createElement("div");
+para.classList.add("book");
+let containerBook = document.getElementById("books-container");
+containerBook.appendChild(para);
+}
+
+
+function button(){
+  addBook(); 
+  render();
+}
+
+
+submit.onclick = button;
+/*
+submit.onclick = addBook; with parenthesses was calling function on load, causing problems
+
+If you don't specify button type as "button", it will reload the page on press (be considered "submit")
+
+*/
