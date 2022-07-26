d39a1f434a3e061865df8cbdf1fc30dc66663946
oberonSilni <sinadamov@gmail.com>
2022-07-27 01:27:07 +0200

    render implemented

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let myLibrary = [];
 
-function Book(author, title, nrOfPages) {
+function Book(author, title, pages) {
     this.author = author,
     this.title = title,
     this.nrOfPages = pages
@@ -46,11 +46,49 @@ close.onclick = function(){
   modal.style.display = "none";
 }
 
+
+//render books on page
+
+
+
+let counter = 0;
+
 function render(){
-let para = document.createElement("div");
-para.classList.add("book");
+
+  for (let i = 0; i <= myLibrary.length - 1; i++) {
+    
+if(counter <= i){
+
+let bookBox = document.createElement("div");
+bookBox.classList.add("book");
 let containerBook = document.getElementById("books-container");
-containerBook.appendChild(para);
+containerBook.appendChild(bookBox);
+
+
+let author = document.createElement("p");
+author.textContent = myLibrary[i].author;
+bookBox.appendChild(author);
+
+let line = document.createElement("hr");
+bookBox.appendChild(line);
+
+let title = document.createElement("p");
+title.textContent = myLibrary[i].title;
+bookBox.appendChild(title);
+
+let line2 = document.createElement("hr");
+bookBox.appendChild(line2);
+
+let pages = document.createElement("p");
+pages.textContent = myLibrary[i].nrOfPages;
+bookBox.appendChild(pages);
+
+
+counter ++;
+}
+
+}
+
 }
 
 
